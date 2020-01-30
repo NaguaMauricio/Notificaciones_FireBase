e4e3cb24ba2495e2f6073eebfdc6f1c8f8e8bc62
NaguaMauricio <56899510+NaguaMauricio@users.noreply.github.com>
2020-01-30 17:43:13 -0500

    Initial commit

---
+++ app/src/main/java/com/example/notificaciones_firebase/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.notificaciones_firebase;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+
+public class MainActivity extends AppCompatActivity {
+    private String TAG = "Noticias";
+    private TextView tok;
+    private Button btn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        FirebaseApp.initializeApp(this);
+        tok = findViewById(R.id.txt_token);
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "getInstanceId failed", task.getException());
+                            return;
+                        }
+                        String token = task.getResult().getToken();
+                        tok.setText(token);
+                        Log.d(TAG, token);
+
+                    }
+                });
+
+
+    }
+
+
+}
